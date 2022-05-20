0ef9327c85c4c61bc3267b63c539ae2bc231e088
Nicolò Pavan <74107738+MagicNicz@users.noreply.github.com>
2022-05-20 11:28:31 +0200

    Update Arduino.ino

--- Arduino.ino
+++ Arduino.ino
@@ -1,6 +1,8 @@
+//IMPORTAZIONE DELLE LIBRERIE UTILIZZATE
 #include <ArduinoJson.h>
 #include "DHT.h"
 
+//DEFINIZIONE DEI PIN PER DHT11 E VENTOLA
 #define DHTPIN 2
 #define DHTTYPE DHT11
 #define fanPin 13
@@ -10,10 +12,10 @@ String message = "";
 bool messageReady = false;
 DHT dht(DHTPIN, DHTTYPE);
 
-int photocellPin = A0;     // the cell and 10K pulldown are connected to a0
+//DICHIARAZIONE PIN ANALOGICI
+int photocellPin = A0;
 int gasPin = A1;
 int groundPin = A2;
-     // the analog reading from the sensor divider
 
 
 void setup() {
@@ -23,21 +25,21 @@ void setup() {
 
 void loop() {
 
-  //GAS
+  //QUALITÀ ARIA
   int gas = analogRead(gasPin); 
 
-  //FOTORESISTENZA
+  //LUMINOSITÀ
   int brightness = analogRead(photocellPin);  
   brightness = 1023 - brightness;
 
-  //DHT11
+  //TEMPERATURA/UMIDITÀ
   int temperature = dht.readTemperature();
   int humidity = dht.readHumidity();
 
-  //TERRENO
+  //SUOLO
   int ground = analogRead(groundPin);
-  ground = constrain(ground,400,1023);  //Keep the ranges!
-  ground = map(ground,400,1023,100,0);  //Map value : 400 will be 100 and 1023 will be 0
+  ground = constrain(ground,400,1023);
+  ground = map(ground,400,1023,100,0);
 
   //ACCENSIONE/SPEGNIMENTO VENTOLA
   if (temperature>=23){
@@ -48,12 +50,12 @@ void loop() {
   }
   
   
-  // Monitor serial communication
+  //COMUNICAZIONE SERIALE CON ARDUINO JSON
   while(Serial.available()) {
     message = Serial.readString();
     messageReady = true;
   }
-  // Only process message if there's one
+  //INVIO MESSAGGIO SE PRESENTE
   if(messageReady) {
     // The only messages we'll parse will be formatted in JSON
     DynamicJsonDocument doc(1024); // ArduinoJson version 6+
